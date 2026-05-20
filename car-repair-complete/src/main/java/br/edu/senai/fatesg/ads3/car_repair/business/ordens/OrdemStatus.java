66f047c51a3f4101df38358e3cec7ebbab5584e3
Filipi Vieira <filipivieiragiaconete@gmail.com>
2026-05-20 18:53:52 -0300

    Delete car-repair-complete directory

--- car-repair-complete/src/main/java/br/edu/senai/fatesg/ads3/car_repair/business/ordens/OrdemStatus.java
+++
@@ -1,9 +0,0 @@
-package br.edu.senai.fatesg.ads3.car_repair.business.ordens;
-
-public enum OrdemStatus {
-    ABERTA,
-    EM_ANDAMENTO,
-    AGUARDANDO_PECAS,
-    CONCLUIDA,
-    CANCELADA
-}
--- car-repair-complete/src/main/java/br/edu/senai/fatesg/ads3/car_repair/business/servicos/ServicoService.java
+++
@@ -1,8 +0,0 @@
-package br.edu.senai.fatesg.ads3.car_repair.business.servicos;
-
-import br.edu.senai.fatesg.ads3.car_repair.core.services.GenericService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ServicoService extends GenericService<ServicoModel, IServicoRepository, ServicoValidation> {
-}
