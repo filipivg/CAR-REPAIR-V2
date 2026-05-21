dc994896569b5d87872c567d04ea8943c3cbb028
Filipi Vieira <filipivieiragiaconete@gmail.com>
2026-05-21 14:47:38 -0300

    Delete src directory

--- src/main/java/br/edu/senai/fatesg/ads3/car_repair/core/exceptions/Severity.java
+++
@@ -1,8 +0,0 @@
-package br.edu.senai.fatesg.ads3.car_repair.core.exceptions;
-
-public enum Severity {
-	INFO, // Informativo
-	WARNING, // Alerta (regra de negócio contornável)
-	ERROR, // Erro impeditivo
-	FATAL // Erro crítico de sistema
-}
